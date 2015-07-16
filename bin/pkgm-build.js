8988ac0105051509416b84dce039eac2e17a5853
Aaron O'Mullan <aaron.omullan@gmail.com>
2015-07-16 23:38:37 +0200

    Initial commit

---
+++ bin/pkgm-build.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var path = require('path');
+var pkgm = require('pkgm');
+var log = console.log.bind(console);
+
+
+// Get target folder to build
+if(process.argv.length < 3) {
+    console.error('Please specify input folder');
+    process.exit(-1);
+}
+var targetFolder = path.resolve(process.argv[2]);
+
+var m = new pkgm({
+    engine: "codebox",
+});
+
+m.loadPackage(targetFolder)
+.then(function(pkg) {
+    return pkg.installDependencies()
+    .then(function(pkg) {
+        return pkg.optimizeClient();
+    });
+})
+.then(function() {
+    log("Done :)");
+})
+.fail(function(err) {
+    log("Failed :(");
+    log(err);
+});
